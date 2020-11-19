cfaddbfcd09d4e326c5eaacea3978234b7a6f77e
juanseromo <juanseromo12@gmail.com>
2020-11-18 22:17:19 -0500

    handleSubmit button in process

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import FormInput from "../form-input/form-input.component";
 import './sign-in.styles.scss';
 
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -15,9 +15,16 @@ class SignIn extends React.Component {
         }
     };
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' })
+        const { email, password} = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' })
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     handleChange = event => {
@@ -43,7 +50,7 @@ class SignIn extends React.Component {
                     <FormInput
                         name='password'
                         type='password'
-                        value={this.state.email}
+                        value={this.state.password}
                         handleChange={this.handleChange}
                         label='password'
                         required
